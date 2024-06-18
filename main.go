751e58eaeef5715a313ac71e9676246e98d8fd4d
Sandeep B <124546985+sandeepbegudem@users.noreply.github.com>
2024-06-18 08:24:32 -0500

    Update main.go

--- main.go
+++ main.go
@@ -6,35 +6,24 @@ import (
 )
 
 func main() {
-	fmt.Println("Welcome to our flight booking application")
-	fmt.Println("Book your joy rides here")
 
-	var command = "go run <file name ex. main.go>"
-	fmt.Println("to run the go file run this command : ", command)
+	bookingClass := "PREMIUM CLASS"
 
-	const myNumber = 7
+	var greeting string = "Hi there!.. Greeting of the day!\n"
+	fmt.Print(greeting)
 
-  // declaring a string variable in the shortest form
-	bookingClass := "FIRST CLASS" // this can be also written in verbose like this - var bookingClass string = "FIRST CLASS"
-
-	// myNumber = 28 cannot resign a variable declared with const
-	fmt.Println(myNumber)
-
-	// const value can't be reassigned
-	const serviceName = "GO Beach Rides"
+	const serviceName = "Amzing Rides"
 
 	var totalSeats uint = 50
 	bookings := []string{}
+
+	greetUsers()
 
 	for {
 		var firstName string
 		var lastName string
 		var email string
 		var availableSeats uint
-
-		// var bookings [50]string
-
-		// Slices don't require declaring the size
 
 		fmt.Print("\nwhat's your first name?\n")
 		fmt.Scan(&firstName)
@@ -48,35 +37,51 @@ func main() {
 		fmt.Print("\nHow many tickets you'd like to book today?\n")
 		fmt.Scan(&availableSeats)
 
-		totalSeats = totalSeats - availableSeats
-		// bookings[0] = firstName + " " + lastName
-		bookings = append(bookings, firstName+" "+lastName)
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@mail.com")
+		isValidTicketEntry := availableSeats > 0 && availableSeats <= totalSeats
 
-		fmt.Printf("\nThank you %v for your booking. You will recieve the booking confirmation to your email : %v \n", firstName, email)
-		fmt.Printf("\nHello %v. Your booking details are below. \nThank you for Choosing us.\n", firstName)
+		if isValidName && isValidEmail && isValidTicketEntry {
+			totalSeats = totalSeats - availableSeats
+			bookings = append(bookings, firstName+" "+lastName)
 
-		fmt.Printf("\nBooking Class : %v \nTotal tickets booked : %v \n", bookingClass, availableSeats)
-		fmt.Printf("\nTotal Available Seats: %v\n", totalSeats)
+			fmt.Printf("\nThank you %v for your booking. You will recieve the booking confirmation to your email : %v \n", firstName, email)
+			fmt.Printf("\nHello %v. Your booking details are below. \nThank you for Choosing us.\n", firstName)
 
-		// fmt.Printf("Printing whole slice of bookings: %v \n", bookings)
-		// fmt.Printf("First Booking: %v\n", bookings[0])
-		// fmt.Printf("Slice Type: %T\n", bookings)
-		// fmt.Printf("Slice length: %v\n", len(bookings))
+			fmt.Printf("\nBooking Class : %v \nTotal tickets booked : %v \n", bookingClass, availableSeats)
+			fmt.Printf("\nTotal Available Seats: %v\n", totalSeats)
 
-		// Go warns you even before executing this program about the Index of 52 is out of bounds
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
 
-		//  fmt.Printf("Printing the index gt. then the actual fixed array value: %v", bookings[52])
+			fmt.Printf("These are all the bookings : %v\n", firstNames)
 
-		// Slices are more convineient and dynamic and we don't need to use the index of the element
-
-		firstNames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
+			if totalSeats == 0 {
+				fmt.Print("We regret to inform you there are no more bookings available, please come back next time!\n")
+				fmt.Print("We appreciate your patience!\n")
+				break
+			}
+		} else {
+			if !isValidName {
+				fmt.Println("your first name and or last name is too short. Ex: john")
+			}
+			if !isValidEmail {
+				fmt.Println("email address you entered is invalid. Ex: doe@mail.com")
+			}
+			if !isValidTicketEntry {
+				fmt.Println("enter a valid number of tickets. Ex: 5")
+			}
+			fmt.Printf("your input data is invalid, please check and try again later!")
+			continue
 		}
-
-		fmt.Printf("These are all the bookings : %v\n", firstNames)
 
 	}
 
+}
+
+func greetUsers() {
+	fmt.Println("Hello, user welcome to our rides ticket booking system.")
 }
